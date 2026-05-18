97be3802b0f05471bad5caffc4d9d796a2825cc0
[Iker] <iker.valverde2006@gmail.com>
2026-05-18 11:44:25 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['carrito'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LC Quiromasajes | Centro de Terapias y Bienestar</title>
-    <script src="../assets/js/script.js"></script>
+    <script src="assets/js/script.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
