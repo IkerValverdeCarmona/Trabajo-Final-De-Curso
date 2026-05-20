08fc12b3395ad16e3c48a3335add3f7c66f08975
[Iker] <iker.valverde2006@gmail.com>
2026-05-20 09:11:21 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['carrito'])) {
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/assets/css/style.css?v=1779260873">
 </head>
 <body>
     <header class="navbar-custom">
